f36c66953e1034a95c210b369408c5db98e492b1
Victor Learned <victorlearned@gmail.com>
2019-01-14 21:22:06 -0800

    Passing props down to Details
    Setting componentDidMount to pull API data
    Setting up rendering

--- src/Details.js
+++ src/Details.js
@@ -1,8 +1,67 @@
 import React from "react";
+import pf from "petfinder-client";
+import { navigate } from "@reach/router";
+
+const petfinder = pf({
+  key: process.env.API_KEY,
+  secret: process.env.API_SECRET
+});
 
 class Details extends React.Component {
+  constructor(props) {
+    super(props);
+    // setting loading to true so user knows when we first load a component it's waiting
+    // on the data from the APi to return. Once we get the data set to false
+    this.state = {
+      loading: true
+    };
+  }
+
+  componentDidMount() {
+    petfinder.pet
+      .get({
+        output: "full",
+        id: this.props.id
+      })
+      .then(data => {
+        const pet = data.petfinder.pet;
+        let breed;
+        if (Array.isArray(pet.breeds.breed)) {
+          breed = pet.breeds.breed.join(", ");
+        } else {
+          breed = pet.breeds.breed;
+        }
+
+        this.setState({
+          name: pet.name,
+          animal: pet.animal,
+          location: `${pet.contact.city}, ${pet.contact.state}`,
+          description: pet.description,
+          media: pet.media,
+          breed,
+          loading: false
+        });
+      })
+      .catch(() => {
+        //   this.setState({ error: err });
+        navigate("/");
+      });
+  }
   render() {
-    return <h1>hi lol</h1>;
+    if (this.state.loading) {
+      return <h1>Loading....</h1>;
+    }
+    const { animal, breed, location, description } = this.state;
+
+    return (
+      <div className="details">
+        <div>
+          <h1>{name}</h1>
+          <h2>{`${animal}-${breed}-${location}`}</h2>
+          <p>{description}</p>
+        </div>
+      </div>
+    );
   }
 }
 
