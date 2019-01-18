3c0b530adf9e5801cd2fc37977e21b0cc8cdc3d2
Victor Learned <victorlearned@gmail.com>
2019-01-17 22:27:30 -0800

    Building out form for searching animals

--- src/SearchParams.js
+++ src/SearchParams.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { ANIMALS } from "petfinder-client";
 
 class SearchParams extends React.Component {
   state = {
@@ -6,17 +7,47 @@ class SearchParams extends React.Component {
     animal: "",
     breed: ""
   };
+  // Creating data binding to the input
+  handleLocationChange = event => {
+    this.setState({
+      location: event.target.value
+    });
+  };
+  handleAnimalChange = event => {
+    this.setState({
+      animal: event.target.value
+    });
+  };
   render() {
     return (
       <div className="search-params">
-        <label htmlFor="location">
-          Location
-          <input
-            id="location"
-            value={this.state.location}
-            placeholder="Location"
-          />
-        </label>
+        <form>
+          <label htmlFor="location">
+            Location
+            <input
+              id="location"
+              value={this.state.location}
+              placeholder="Location"
+              onChange={this.handleLocationChange}
+            />
+          </label>
+          <label htmlFor="animal">
+            Animal
+            <select
+              id="animal"
+              value={this.state.animal}
+              onChange={this.handleAnimalChange}
+              onBlur={this.handleAnimalChange}
+            >
+              <option />
+              {ANIMALS.map(animal => (
+                <option key={animal} value={animal}>
+                  {animal}
+                </option>
+              ))}
+            </select>
+          </label>
+        </form>
       </div>
     );
   }
